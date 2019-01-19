2b4e87e0d089a986b160316eec052cb7dcd4b66a
Eyal Wiener <eyalwiener@gmail.com>
2019-01-19 18:41:03 +0200

    Added background.js to same code chunk as foreground script

--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import { runBackgroundScript } from './core/backgroundMain';
-// import { isBackgroundScript } from './core/utils';
-// import { ErrorBoundary } from './bugsnag';
+import { runBackgroundScript } from './core/backgroundMain';
+import { isBackgroundScript } from './core/utils';
+import { ErrorBoundary } from './bugsnag';
 
-// if (isBackgroundScript()) {
-// runBackgroundScript();
-// } else {
-const rootEl = document.getElementById('root');
+if (isBackgroundScript()) {
+  runBackgroundScript();
+} else {
+  const rootEl = document.getElementById('root');
 
-if (!rootEl) {
-  throw new Error('React root element is missing');
+  if (!rootEl) {
+    throw new Error('React root element is missing');
+  }
+
+  ReactDOM.render(
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>,
+    rootEl
+  );
 }
-
-ReactDOM.render(
-  // <ErrorBoundary>
-  <App />,
-  // </ErrorBoundary>,
-  rootEl
-);
-// }
