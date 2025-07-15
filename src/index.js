04cd0e17b8a40e7ea6e7c7b329117df49b074626
Emily Fan <emilyfan@mit.edu>
2025-07-14 20:17:56 -0700

    split background from index

--- src/index.js
+++ src/index.js
@@ -2,24 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { runBackgroundScript } from './core/backgroundMain';
-import { isBackgroundScript } from './core/utils';
 import { ErrorBoundary } from './bugsnag';
 
-if (isBackgroundScript()) {
-  runBackgroundScript();
-} else {
-  const rootEl = document.getElementById('root');
-  rootEl.style.width = "max-content";
+const rootEl = document.getElementById('root');
+rootEl.style.width = "max-content";
 
-  if (!rootEl) {
-    throw new Error('React root element is missing');
-  }
-
-  ReactDOM.render(
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>,
-    rootEl
-  );
+if (!rootEl) {
+  throw new Error('React root element is missing');
 }
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  rootEl
+);
