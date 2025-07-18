7ce003dffc486a044177e742c4250b33131dea85
Emily Fan <hello@emilyf.com>
2025-07-18 15:05:12 -0700

    use flow-typed moment instead

--- flow-typed/moment.js
+++ flow-typed/moment.js
@@ -1,3 +1,45 @@
-declare module 'moment' {
-  declare export default any;
-}
+// declare module 'moment' {
+//   declare export default function moment(inp?: any, strict?: boolean): Moment;
+
+//   declare export var weekdaysShort: () => string[];
+
+//   declare export class Moment {
+//     format(format?: string): string;
+
+//     startOf(unitOfTime: any): Moment;
+//     endOf(unitOfTime: any): Moment;
+
+//     add(amount?: any, unit?: any): Moment;
+
+//     calendar(): string;
+
+//     day(d: number|string): Moment;
+//     day(): number;
+
+//     hour(h: number): Moment;
+//     hour(): number;
+//     hours(h: number): Moment;
+//     hours(): number;
+
+//     minute(m: number): Moment;
+//     minute(): number;
+//     minutes(m: number): Moment;
+//     minutes(): number;
+
+//     second(s: number): Moment;
+//     second(): number;
+//     seconds(s: number): Moment;
+//     seconds(): number;
+
+//     millisecond(ms: number): Moment;
+//     millisecond(): number;
+//     milliseconds(ms: number): Moment;
+//     milliseconds(): number;
+    
+//     toDate(): Date;
+
+//     isBefore(inp?: any, granularity?: any): boolean;
+
+//     weekdaysShort(): string[];
+//   };
+// }
